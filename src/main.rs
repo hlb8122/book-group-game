c060f4bf721e0b56ca3bce04f348190495b5b06e
Harry Barber <harrybarber@protonmail.com>
2022-01-27 23:36:45 +0000

    End of session

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use bevy::{prelude::*, sprite::collide_aabb::{Collision, collide}};
+use bevy::{
+    prelude::*,
+    sprite::collide_aabb::{collide, Collision},
+};
 
 // Component | NA        | Component | NA | Component
 // Component | Component | Component | NA | Component
 
-#[derive(Default, Component)]
+#[derive(Default, Clone, Component)]
 pub struct Velocity(pub Vec2);
 
 #[derive(Component)]
@@ -54,29 +57,38 @@ pub fn wall_bounce(mut queries: Query<(&Transform, &mut Velocity)>) {
 }
 
 pub fn collision(
-    paddle: Query<(&Transform, &Sprite), With<PaddleMarker>>,
+    paddle: Query<(&Transform, &Sprite, &Velocity), (With<PaddleMarker>, Without<BallMarker>)>,
     mut ball: Query<(&Transform, &Sprite, &mut Velocity), With<BallMarker>>,
 ) {
-    for (paddle_transform, paddle_sprite) in paddle.iter() {
-        for (ball_transform, sprite, ball_velocity) in paddle.iter_mut() {
-            let sprite_size: Vec2 = ;
-            let ball_size: Vec2;
+    for (paddle_transform, paddle_sprite, paddle_velocity) in paddle.iter() {
+        for (ball_transform, ball_sprite, mut ball_velocity) in ball.iter_mut() {
+            let sprite_size: Vec2 = paddle_sprite.custom_size.expect("custom sized");
+            let ball_size: Vec2 = ball_sprite.custom_size.expect("custom sized");
 
-            let collision: Collision = collide(
+            let opt_collision = collide(
                 paddle_transform.translation,
                 sprite_size,
                 ball_transform.translation,
                 ball_size,
             );
 
+            let collision = if let Some(some) = opt_collision {
+                some
+            } else {
+                continue;
+            };
+
             use Collision::*;
             let multiplier = match collision {
                 Top => Vec2::new(1., -1.),
-                Bottom => Vec::new(1., -1.),
-                Left => Vec::new(-1., 1.),
-                Right => Vec::new(-1, 1.)
+                Bottom => Vec2::new(1., -1.),
+                Left => Vec2::new(-1., 1.),
+                Right => Vec2::new(-1., 1.),
             };
-            ball_velocity.0 *= 
+            ball_velocity.0 *= multiplier;
+
+            // Violate conservation of momentum because hey
+            ball_velocity.0 += paddle_velocity.clone().0 * 1.01;
         }
     }
 }
@@ -92,20 +104,22 @@ fn keyboard_input(keys: Res<Input<KeyCode>>, mut query: Query<&mut Velocity, Wit
 
     let mut velocity = query.single_mut();
 
-    if keys.just_pressed(KeyCode::W) {
-        velocity.0 = Vec2::new(0., SPEED);
+    *velocity = Velocity::default();
+
+    if keys.pressed(KeyCode::W) {
+        velocity.0 += Vec2::new(0., SPEED);
     }
 
-    if keys.just_pressed(KeyCode::A) {
-        velocity.0 = Vec2::new(-SPEED, 0.);
+    if keys.pressed(KeyCode::A) {
+        velocity.0 += Vec2::new(-SPEED, 0.);
     }
 
-    if keys.just_pressed(KeyCode::S) {
-        velocity.0 = Vec2::new(0., -SPEED);
+    if keys.pressed(KeyCode::S) {
+        velocity.0 += Vec2::new(0., -SPEED);
     }
 
-    if keys.just_pressed(KeyCode::D) {
-        velocity.0 = Vec2::new(SPEED, 0.);
+    if keys.pressed(KeyCode::D) {
+        velocity.0 += Vec2::new(SPEED, 0.);
     }
 }
 
@@ -158,5 +172,6 @@ fn main() {
         .add_system(kinematics)
         .add_system(keyboard_input)
         .add_system(wall_bounce)
+        .add_system(collision)
         .run();
 }
